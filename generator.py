145765ed383ad68f21e83ff9e58029ebd1488965
nyildirim <nyildirim@protonmail.com>
2022-10-16 23:25:29 -0400

    fixed lint issues

--- generator.py
+++ generator.py
@@ -1,3 +1,8 @@
+"""
+Collection of the functions where they are being used to generate
+the question and its answer to be served with the redered questions
+"""
+
 import ipaddress
 import random
 
@@ -5,46 +10,46 @@ MAX_IPV4 = ipaddress.IPv4Address._ALL_ONES
 
 def random_ipv4():
     """
-    This function returns random IPv4 address which is only public or private.
+    This function returns random IPv4 address
+    which is only can be public or private.
     """
-    randomIp = ipaddress.IPv4Address(random.randint(0, MAX_IPV4))
-    while randomIp.is_private is False and randomIp.is_global is False:
-        randomIp = ipaddress.IPv4Address(random.randint(0, MAX_IPV4))
-    return  f"{randomIp}/{random.randint(3, 29)}"
+    random_ip = ipaddress.IPv4Address(random.randint(0, MAX_IPV4))
+    while random_ip.is_reserved is True :
+        random_ip = ipaddress.IPv4Address(random.randint(0, MAX_IPV4))
+        continue
+    return  f"{random_ip}/{random.randint(3, 29)}"
 
-def firstIp(IP_Addr):
+def first_ip(ip_addr):
     """
-    This is the Question 4's answer function. 
-    It declares the net address and prints the first avaliable ipv4 host address
+    Returns first avaliable ipv4 host address
     """
-    net = ipaddress.ip_network(IP_Addr, strict=False)
+    net = ipaddress.ip_network(ip_addr, strict=False)
     return net[1]
 
-def lastIp(IP_Addr):
-
-    net = ipaddress.ip_network(IP_Addr, strict=False)
+def last_ip(ip_addr):
+    """
+    Returns  last avaliable ipv4 host address
+    """
+    net = ipaddress.ip_network(ip_addr, strict=False)
     return net[-2]
 
-def numberSubnet(IP_Addr):
-    net = ipaddress.ip_network(IP_Addr, strict=False)
-    return net(IP_Addr).subnets()
-
-
-
-def main(questionKind, random_ipv4):
+def number_subnet(ip_addr):
     """
-    This is the main function for choosing the write answer for the questions. 
-    Depending on the question type, it selects the function.
+    Returns number of avaliable subnets
     """
-    if questionKind == 4:
-        return firstIp(random_ipv4)
-    elif questionKind == 6:
-        return f'{firstIp(random_ipv4)} - {lastIp(random_ipv4)}'
-    elif questionKind ==3:
-        return lastIp(random_ipv4)
-    elif questionKind == 5:
-        return numberSubnet(random_ipv4)
+    net = ipaddress.ip_network(ip_addr, strict=False)
+    return net(ip_addr).subnets()
 
-
- 
-
+def q_maker(question_kind, random_ip):
+    """
+    This is the main function for choosing the right answer for
+    the questions. Depending on the question type, it selects the function.
+    """
+    if question_kind == 4:
+        return first_ip(random_ip)
+    if question_kind == 6:
+        return f'{first_ip(random_ip)} - {last_ip(random_ip)}'
+    if question_kind ==3:
+        return last_ip(random_ip)
+    if question_kind == 5:
+        return number_subnet(random_ip)
--- subapp.py
+++ subapp.py
@@ -1,34 +1,42 @@
+"""
+This is the main flask route module where its function defined along 
+with its route. The logic is defined under the question function.
+
+"""
+
+from random import randint
 from flask import Flask, render_template
 import yaml
-from random import randint
 from jinja2 import Template
-from generator import *
+from generator import q_maker, random_ipv4, ipaddress
 
 app = Flask(__name__)
 
-
 @app.route("/")
 
-def question():
+def question() :
 
-    with open(r'questionlist.yml') as file:
+    """
+    
+    This generates a random parameter for the question and renders it with
+    the yaml file under questionlist then serves the question with its answer.
 
-        questionNumber = randint(4, 4)
-        questionlist = yaml.full_load(file)
-        randomIpAddr = ipaddress.ip_interface(random_ipv4())
+    """
 
-        dataRandomizer = {
-            "hostAddress": randomIpAddr,
+    with open(r'questionlist.yml', encoding="utf-8") as file:
+
+        q_number = randint(4, 4)
+        q_list = yaml.full_load(file)
+        random_ip_addr = ipaddress.ip_interface(random_ipv4())
+
+        q_parameters = {
+            "hostAddress": random_ip_addr,
         }
+        j2_template = Template(q_list['Questions']['Kinds'][q_number])        
+        renderred_question = j2_template.render(q_parameters)
+        answer = q_maker(q_number, random_ip_addr)
 
-        j2_template = Template(questionlist['Questions']['Kinds'][questionNumber])        
-        question = j2_template.render(dataRandomizer)
-
-        
-        answer = main(questionNumber, random_ipv4=randomIpAddr)
-
-
-    return render_template('home.html', question = question, answer = answer)
+    return render_template('home.html', question = renderred_question, answer = answer)
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
