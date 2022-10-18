a53f4028e4ac2f9e75cb9c4018a6313ea59aea4d
nyildirim <nyildirim@protonmail.com>
2022-10-17 23:24:14 -0400

    added new questions

--- generator.py
+++ generator.py
@@ -33,23 +33,48 @@ def last_ip(ip_addr):
     net = ipaddress.ip_network(ip_addr, strict=False)
     return net[-2]
 
-def number_subnet(ip_addr):
+def brodcast_ip(ip_addr):
+    """
+    Returns  last avaliable ipv4 host address
+    """
+    net = ipaddress.ip_network(ip_addr, strict=False)
+    return net[-1]
+
+def number_host(ip_addr):
+    return 2**(32 - int(str(ip_addr).partition('/')[-1])) - 2
+
+def max_subnet(ip_addr):
     """
     Returns number of avaliable subnets
     """
-    net = ipaddress.ip_network(ip_addr, strict=False)
-    return net(ip_addr).subnets()
+    return 2**(32 - int(str(ip_addr).partition('/')[-1])) / 2
 
-def q_maker(question_kind, random_ip):
+
+def max_host(ip_addr):
+    """
+    Returns number of avaliable subnets
+    """
+    return (2**(32 - int(str(ip_addr).partition('/')[-1])) / 2) - 2
+
+def q_maker(question_kind, random_ip, random_net, *args, **kwargs):
     """
     This is the main function for choosing the right answer for
     the questions. Depending on the question type, it selects the function.
     """
+
+    if question_kind == 1:
+        return number_host(random_net) 
+    if question_kind == 2:
+        return brodcast_ip(random_net) 
+    if question_kind == 3:
+        return last_ip(random_ip)
     if question_kind == 4:
         return first_ip(random_ip)
-    if question_kind == 6:
-        return f'{first_ip(random_ip)} - {last_ip(random_ip)}'
-    if question_kind ==3:
-        return last_ip(random_ip)
     if question_kind == 5:
-        return number_subnet(random_ip)
+        return f'{first_ip(random_ip)} - {last_ip(random_ip)}'
+    if question_kind == 6:
+        return max_subnet(random_ip)
+    if question_kind == 7:
+        return max_host(random_ip)
+
+
