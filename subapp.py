819def71d27924ef7339731f7f8dd105ae9475c3
nyildirim <nyildirim@protonmail.com>
2022-10-17 23:24:34 -0400

    added new questions

--- subapp.py
+++ subapp.py
@@ -21,16 +21,19 @@ def question() :
 
     with open(r'questionlist.yml', encoding="utf-8") as file:
 
-        q_number = randint(4, 4)
+        q_number = randint(2, 2)
         q_list = yaml.full_load(file)
         random_ip_addr = ipaddress.ip_interface(random_ipv4())
+        random_net_addr = ipaddress.ip_network(random_ipv4(), strict=False)
 
         q_parameters = {
             "hostAddress": random_ip_addr,
+            "networkAddress": random_net_addr
         }
-        j2_template = Template(q_list['Questions']['Kinds'][q_number])        
+
+        j2_template = Template(q_list['Questions'][q_number])    
         renderred_question = j2_template.render(q_parameters)
-        answer = q_maker(q_number, random_ip_addr)
+        answer = q_maker(question_kind=q_number, random_ip=random_ip_addr, random_net=random_net_addr)
 
     return render_template('home.html', question = renderred_question, answer = answer)
 
